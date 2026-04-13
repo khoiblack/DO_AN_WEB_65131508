3495547ac2641199468449db73d944de7d2a01bb
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-13 15:14:06 +0700

    update code

---
+++ BookingTourWeb/BookingTourWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingTourWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
