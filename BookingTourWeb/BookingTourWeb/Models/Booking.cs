d509bb1cff5067b4cddc47cdde9491187db068b9
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-13 14:24:57 +0700

    tạo bảng booking

---
+++ BookingTourWeb/BookingTourWeb/Models/Booking.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingTourWeb.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên khách hàng")]
+        public string CustomerName { get; set; } = "";
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime DepartureDate { get; set; } // Ngày đi
+
+        [Required]
+        public int NumberOfGuests { get; set; } // Số lượng khách
+
+        public decimal TotalPrice { get; set; } // Tổng tiền
+
+        public string Status { get; set; } = "Chờ duyệt"; // Trạng thái đơn (Chờ duyệt, Đã thanh toán, Đã hủy)
+
+        // Khóa ngoại (Foreign Key) liên kết với bảng Tour
+        public int TourId { get; set; }
+        [ForeignKey("TourId")]
+        public Tour? Tour { get; set; }
+    }
+}
