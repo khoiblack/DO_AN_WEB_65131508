eb929375e61a2a912fa373d8b33e1d56048e9879
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-13 14:04:17 +0700

    create 2 table

---
+++ BookingTourWeb/BookingTourWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookingTourWeb.Models; // Đổi tên project của bạn nếu cần
+
+namespace BookingTourWeb.Data
+{
+    // Kế thừa từ DbContext của Entity Framework
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Khai báo 2 bảng sẽ được tạo trong SQL Server
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
