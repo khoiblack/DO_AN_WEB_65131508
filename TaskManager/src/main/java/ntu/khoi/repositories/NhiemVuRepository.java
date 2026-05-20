34ed75faf0aa16123f9112c16dc7ad776501e50b
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-05-20 14:56:48 +0700

    update code

--- TaskManager/src/main/java/ntu/khoi/repositories/NhiemVuRepository.java
+++ TaskManager/src/main/java/ntu/khoi/repositories/NhiemVuRepository.java
@@ -1,5 +1,15 @@
 package ntu.khoi.repositories;
 
-public class NhiemVuRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import ntu.khoi.models.NhiemVu;
 
+@Repository
+public interface NhiemVuRepository extends JpaRepository<NhiemVu, Integer> {
+    
+    List<NhiemVu> findByDuAn_Id(Integer id);
+    
+    
+    List<NhiemVu> findByNguoiThucHien_Id(Integer id);
 }
