f14510b6d87fadc54fc4dbeaa06fbb5ac3d9baff
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-05-20 14:24:27 +0700

    delete

--- BookingTourWeb/BookingTourWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookingTourWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
